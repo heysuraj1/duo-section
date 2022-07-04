c518920ec648f3c5df9d7573e8392cacd12a73be
Kumar Nitesh <kumarniteshoffice786@gmail.com>
2022-07-04 12:26:21 +0530

    made some chnages

---
+++ js.js
@@ -0,0 +1,26 @@
+// data = '[{"name" : "Riyaz"},{"name" : "Javed"},{"name" : "Arun"},{"name" : "Sunil"},{"name" : "Rahul"},{"name" : "Anita"}]';
+
+
+
+
+data = `{
+    "Text_Value": {
+        "text": "h5",
+        "color": "white",
+        "fontSize": "5px",
+        "margine": "2px",
+        "padding": "2px",
+        "font-Family": "Verdana, Geneva, Tahoma, sans-serif",
+        "font-weight":"bold",
+        "position":"absolute",
+        "value1":"Kumar Nitesh",
+        "value2":"Rathyatra Varanasi",
+        "value3":"6393344720"
+
+    },
+    "Logo":{
+        "uri":"someimageurl.jpeg",
+        "size":"50px"
+    }
+    
+}`
